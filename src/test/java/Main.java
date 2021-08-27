6a6c93d0e9ed6a1d6d6cd4addfd490bee0b53df5
Romaindu35 <67557705+Romaindu35@users.noreply.github.com>
2021-08-27 09:56:16 +0200

    Update test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,15 +7,8 @@ import java.net.URI;
 public class Main {
 
     public static void main(String[] args) throws PufferException {
-        PufferJavaAPI pufferJavaAPI = new PufferJavaAPI(new Authentificator(URI.create("https://puffer.envium.fr/"), "romain.sai-tanguy@sfr.fr", "Romain35"));
-
-        /*System.out.println(pufferJavaAPI.send(pufferJavaAPI.getRequest().getServers()).getServers());
-
-        PermissionView permissionView = pufferJavaAPI.send(pufferJavaAPI.getRequest().getUserAccessServer("ada473e9")).get(0);
-        pufferJavaAPI.send(pufferJavaAPI.getRequest().editUserAccessServerPermission("ada473e9", ""))*/
-
-        System.out.println(pufferJavaAPI.send(pufferJavaAPI.getRequest().isDaemonUp()));
-        pufferJavaAPI.getNodesManager().getServerByName("default");
+        PufferJavaAPI pufferJavaAPI = new PufferJavaAPI(new Authentificator(URI.create("panelIP"), "email", "password"));
+   
 
         System.exit(0);
     }
